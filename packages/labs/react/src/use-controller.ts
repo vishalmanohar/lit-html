6f1cc45de3a0fefbb77be82cc98c3960d3899585
Justin Fagnani <justinfagnani@google.com>
2021-01-26 20:21:43 -0800

    add test

--- packages/labs/react/src/use-controller.ts
+++ packages/labs/react/src/use-controller.ts
@@ -21,103 +21,117 @@ import {
 type React = typeof ReactModule;
 
 export type ControllerConstructor<C extends ReactiveController> = {
-  new (...args: Array<unknown>): C;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  new (...args: Array<any>): C;
 };
 
-export const useController = <C>(
-  React: React,
-  ctor: ControllerConstructor<C>
-) => (...args: ConstructorParameters<typeof ctor>) => {
-  const {useState, useEffect} = React;
+class ReactControllerHost<C extends ReactiveController>
+  implements ReactiveControllerHost {
+  _primaryController!: C;
+  private _controllers: Array<ReactiveController> = [];
+  private _updateCompletePromise: Promise<boolean>;
+  _updatePending = true;
+  _resolveUpdate!: (value: boolean | PromiseLike<boolean>) => void;
+  // _rejectUpdate!: (reason?: any) => void;
 
-  const [kickCount, kick] = useState(0);
+  kickCount: number;
+  kick: (k: number) => void;
 
-  class ReactControllerHost<C extends ReactiveController>
-    implements ReactiveControllerHost {
-    _primaryController: C;
-    private _controllers: Array<ReactiveController> = [];
-    private _updateCompletePromise: Promise<boolean>;
-    _updatePending = true;
-    _resolveUpdate!: (value: boolean | PromiseLike<boolean>) => void;
-    // _rejectUpdate!: (reason?: any) => void;
+  constructor(kickCount: number, kick: (k: number) => void) {
+    this.kickCount = kickCount;
+    this.kick = kick;
+    this._updateCompletePromise = new Promise((res, _rej) => {
+      this._resolveUpdate = res;
+      // this._rejectUpdate = rej;
+    });
+  }
 
-    constructor(controller: C) {
-      this._primaryController = controller;
-      this._updateCompletePromise = new Promise((res, _rej) => {
-        this._resolveUpdate = res;
-        // this._rejectUpdate = rej;
-      });
-    }
+  addController(controller: ReactiveController): void {
+    this._controllers.push(controller);
+  }
 
-    addController(controller: ReactiveController): void {
-      this._controllers.push(controller);
-    }
-
-    requestUpdate(): void {
-      if (this._updatePending === true) {
-        return;
-      }
+  requestUpdate(): void {
+    if (!this._updatePending) {
       this._updatePending = true;
-
       // Trigger a React update by updating some state
-      kick(kickCount + 1);
-
-      this._updateCompletePromise = new Promise((res, _rej) => {
-        this._resolveUpdate = res;
-        // this._rejectUpdate = rej;
-      });
-    }
-
-    get updateComplete(): Promise<boolean> {
-      return this._updateCompletePromise;
-    }
-
-    connected() {
-      this._controllers.forEach((c) => c.connected?.());
-    }
-
-    disconnected() {
-      this._controllers.forEach((c) => c.disconnected?.());
-    }
-
-    willUpdate() {
-      this._controllers.forEach((c) => c.willUpdate?.());
-    }
-
-    update() {
-      this._controllers.forEach((c) => c.update?.());
-    }
-
-    updated() {
-      this._controllers.forEach((c) => c.updated?.());
+      Promise.resolve().then(() => this.kick(this.kickCount + 1));
     }
   }
 
+  get updateComplete(): Promise<boolean> {
+    return this._updateCompletePromise;
+  }
+
+  connected() {
+    this._controllers.forEach((c) => c.connected?.());
+  }
+
+  disconnected() {
+    this._controllers.forEach((c) => c.disconnected?.());
+  }
+
+  willUpdate() {
+    this._controllers.forEach((c) => c.willUpdate?.());
+  }
+
+  update() {
+    this._controllers.forEach((c) => c.update?.());
+  }
+
+  updated() {
+    this._updatePending = false;
+    const resolve = this._resolveUpdate;
+    this._updateCompletePromise = new Promise((res, _rej) => {
+      this._resolveUpdate = res;
+      // this._rejectUpdate = rej;
+    });
+    this._controllers.forEach((c) => c.updated?.());
+    resolve(this._updatePending);
+  }
+}
+
+/**
+ * Creates and stores a stateful ReactiveController instance and provides it
+ * with a ReactiveControllerHost that drives the controller lifecycle.
+ *
+ * Use this hook to convert ReactiveController into a React hook.
+ *
+ * @param React the React module that provides the base hooks. Must provide
+ * `useState` and `useLayoutEffect`.
+ * @param createController A function that creates a controller instance. This
+ * function is given a ReactiveController to pass to the controller. The create
+ * function is only called once per component.
+ */
+export const useController = <C extends ReactiveController>(
+  React: React,
+  createController: (host: ReactiveControllerHost) => C
+): C => {
+  const {useState, useLayoutEffect} = React;
+  const [kickCount, kick] = useState(0);
+
+  // Create and store the controller instance. We use useState() instead of
+  // useMemo() because React does not gaurentee that it will preserve values
+  // created with useMemo().
+  // TODO: since this controller are mutable, this may cause issues such as
+  // "shearing" with React concurrent mode. The solution there will likely be
+  // to shapshot the controller state with something like `useMutableSource`:
+  // https://github.com/reactjs/rfcs/blob/master/text/0147-use-mutable-source.md
+  // We can address this when concurrent mode is closer to shipping.
   const [host] = useState(() => {
-    const host = new ReactControllerHost(new ctor(...args));
-    // is this right, or should it be the first layout effect?
+    const host = new ReactControllerHost<C>(kickCount, kick);
+    const controller = createController(host);
+    host._primaryController = controller;
     host.connected();
     return host;
   });
 
+  host._updatePending = true;
+
+  useLayoutEffect(() => () => host.disconnected(), []);
+  useLayoutEffect(() => host.updated());
   host.willUpdate();
+  // TODO: don't call in SSR
   host.update();
 
-  useEffect(() => {
-    host.updated();
-    if (host._updatePending) {
-      host._resolveUpdate(false);
-      host._updatePending = false;
-    }
-    return () => host.disconnected();
-  });
-
-  // For now, just return the controller instance
-  // It would be very interesting to be able to customize the hook args
-  // and return value so that an OOP controller API, with settable properties,
-  // could be converted to idomatic hooks-style.
-  // Idea: two functions provided to useController. One separates constructor
-  // args from per-render props. Another customizes the return value so it
-  // could return setter functions that set instance properties, etc.
   return host._primaryController;
 };
